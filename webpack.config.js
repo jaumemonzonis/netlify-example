41f1c5dddb63129c2aec392c7cd4b2883d19964e
jaume monzonis <jaume_monzonis@hotmail.com>
2019-11-14 10:35:57 +0100

    webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './'),
+    path: path.resolve(__dirname, './dist'),
     publicPath: '/',
     filename: 'build.js'
   },
